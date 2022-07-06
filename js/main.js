13e65aca2ef08941d76c30911c4db6de254df9ba
Vit Mur <myp1blka@gmail.com>
2022-07-07 00:37:26 +0300

    2st step

--- js/main.js
+++ js/main.js
@@ -1,93 +1,117 @@
 
 const elStartGameButton = document.querySelector('input[name="bStartGame"]'); // стартова кнопка
-const elNumberOfSheepUnits = document.querySelector('input[name="number-of-sheep-units"]');// Кількість монстрів
+const elFireButton = document.querySelector('input[name="bFire"]'); // байрактарна кнопка
+
+const elNumberOfSheepUnits = document.querySelector('input[name="number-of-sheep-units"]');
 
 const elPlayer1field = document.querySelector('.player1field');
 const elPlayer2field = document.querySelector('.player2field');
 const elGameLog = document.querySelector('.gameLog');
 
+let numberOfSheepUnits;
+
 let player1sheeps = [];
 let player2sheeps = [];
 
-
-const StartGameFunction = (e) => {
-
-        let player1HTML = ''; 
-        let player2HTML = ''; 
-        let logHTML = ''; 
-
-        elPlayer1field.innerHTML = '';
-        elPlayer1field.insertAdjacentHTML('beforeend', player1HTML);
-        elPlayer2field.innerHTML = '';
-        elPlayer2field.insertAdjacentHTML('beforeend', player2HTML);
-        elGameLog.innerHTML = '';
-        elGameLog.insertAdjacentHTML('beforeend', logHTML);
-
-        let numberOfSheepUnits =  parseInt( elNumberOfSheepUnits.value );
-
-
-
-        player1sheeps.length = [];
-        player2sheeps.length = [];
+let player1HTML = ''; 
+let player2HTML = ''; 
+let logHTML = ''; 
 
 
 
 
-        for (let i = 0; i < (numberOfSheepUnits); i++) {
 
-            let unitSize = randValue(1, 5, 1);
-            let unitHealth = randValue(10, 100, 1);
-            let unitForce = randValue(10, 100, 1);
-            let unitPower = randValue(10, 100, 1);
-              
+// ================================================================================================
+// Generate Sheeps Player 1
+const GenerateSheepsPlayer1 = (e) => {
+    for (let i = 0; i < (numberOfSheepUnits); i++) {
 
-            let unit = {     // объект
-                size: unitSize,
-                health: unitHealth ,
-                force: unitForce ,
-                power: unitPower 
-              };
+        let unitSize    = randValue(1,    4, 1);
+        let unitHealth  = randValue(10, 100, 1);
+        let unitForce   = randValue(10, 100, 1);
+        let unitPower   = randValue(10, 100, 1);
+            
 
-            player1sheeps.push(unit);
-            //console.log(player1sheeps[i])
-        }
-
-            for (let i = 0; i < (numberOfSheepUnits); i++) {
-                let imageSheeps = `<img src="img/sheep${ randValue(0, 4, 1) }.png" width="32"></img>`;
-                player1HTML += `     <div class="sheep1">
-                Підрозділ номер ${i+1}<br>
-                ${imageSheeps.repeat(player1sheeps[i].size)}
-                <br>
-                Health: ${player1sheeps[i].health } 
-                Force:  ${player1sheeps[i].force } 
-                Power:  ${player1sheeps[i].power }
-                </div>`;
-            }
+        let unit = {  
+            size:   unitSize,
+            health: unitHealth ,
+            force:  unitForce ,
+            power:  unitPower 
+            };
+        player1sheeps.push(unit);
+    }
     
-            for (let i = 0; i < (numberOfSheepUnits); i++) {
-                player2HTML += `     <div class="sheep2">
-                    <img src="img/sheep${ randValue(0, 4, 1) }.png" width="32">
-                </div>`;
-            }
-            logHTML += ` ${player1sheeps[0].health} <br>`;
-            player1sheeps[0].health -= 50;
-            logHTML+= `Мінус 50 балів Слізерину <br>`;
-            logHTML += ` ${player1sheeps[0].health} `;
+}
 
 
+// ================================================================================================
+// Generate Sheeps Player 2
+const GenerateSheepsPlayer2 = (e) => {
+    for (let i = 0; i < (numberOfSheepUnits); i++) {
 
+        let unitSize    = randValue(1,    4, 1);
+        let unitHealth  = randValue(10, 100, 1);
+        let unitForce   = randValue(10, 100, 1);
+        let unitPower   = randValue(10, 100, 1);
+            
 
+        let unit = {  
+            size:   unitSize,
+            health: unitHealth ,
+            force:  unitForce ,
+            power:  unitPower 
+            };
+        player2sheeps.push(unit);
+    }
 
+}
 
-        elPlayer1field.insertAdjacentHTML('beforeend', player1HTML);
-        elPlayer2field.insertAdjacentHTML('beforeend', player2HTML);
-        elGameLog.insertAdjacentHTML('beforeend', logHTML);
+const DrawStepGame = (e) => {
+    player1HTML = ''; 
+    player2HTML = ''; 
+    elPlayer1field.innerHTML = '';
+    elPlayer2field.innerHTML = '';
 
+    for (let i = 0; i < (numberOfSheepUnits); i++) {
+        let imageSheeps = `<img src="img/sheep${ randValue(0, 4, 1) }.png" width="32"></img>`;
 
+        // Player 1
+        player1HTML += `
+        <div class="sheep1">
+        Підрозділ номер ${i+1}<br>
+        ${imageSheeps.repeat(player1sheeps[i].size)}<br>
+        Health: ${player1sheeps[i].health } 
+        Force:  ${player1sheeps[i].force } 
+        Power:  ${player1sheeps[i].power }
+        </div>`;
 
+        // Player 2
+        player2HTML += `
+        <div class="sheep1">
+        Підрозділ номер ${i+1}<br>
+        ${imageSheeps.repeat(player2sheeps[i].size)}<br>
+        Health: ${player2sheeps[i].health } 
+        Force:  ${player2sheeps[i].force } 
+        Power:  ${player2sheeps[i].power }
+        </div>`;
 
     }
 
+    elPlayer1field.insertAdjacentHTML('beforeend', player1HTML);
+    elPlayer2field.insertAdjacentHTML('beforeend', player2HTML);
+    elGameLog.     insertAdjacentHTML('beforeend', logHTML);
+}
+
+const FireFunction = (e) => {
+
+logHTML += ` Підрозділ 1(${player1sheeps[0].health}) <br>`;
+player1sheeps[0].health -= 50;
+logHTML += ` Підрозділ 1 втрачає 50 очок = ${player1sheeps[0].health} `;
+
+DrawStepGame();
+
+
+}
 
 
 
@@ -95,6 +119,33 @@ const StartGameFunction = (e) => {
 
 
 
+
+
+
+
+// ================================================================================================
+// Start Game
+const StartGameFunction = (e) => {
+
+    player1HTML = ''; 
+    player2HTML = ''; 
+    logHTML = ''; 
+
+    elPlayer1field.innerHTML = '';
+    elPlayer2field.innerHTML = '';
+    elGameLog.innerHTML = '';
+
+    numberOfSheepUnits =  parseInt( elNumberOfSheepUnits.value );
+
+    player1sheeps.length = [];
+    player2sheeps.length = [];
+
+    GenerateSheepsPlayer1();
+    GenerateSheepsPlayer2();
+
+    DrawStepGame();
+
+}
 
 //   --------------------------------------------------------------////////////////////-----------------------------------------
 // Рандомна функція (від, до, кратна чому)
@@ -105,3 +156,4 @@ const randValue = (min,max,num) => {
 //   --------------------------------------------------------------////////////////////-----------------------------------------
 // Клік на кнопку
 elStartGameButton.addEventListener('click', () => {StartGameFunction()});
+elFireButton.addEventListener('click', () => {FireFunction()});
